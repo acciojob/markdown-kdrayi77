194086abda99e31fbd3eb3a4fae32f43343535a6
kdrayi77 <120662678+kdrayi77@users.noreply.github.com>
2025-10-29 20:31:57 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,73 +1,14 @@
-import React,{useState,useEffect} from "react";
-import ReactMarkdown from "react-markdown";
-import '../styles/App.css'
+import React from "react";
+import MarkdownEditor from "./MarkdownEditor";
+import "./styles.css";
 
-function App(){
-
-  const [markdown,setMarkdown] = useState("")
-  const [loading,setLoading] = useState(true)
-
-  const handleChange = (e) =>{
-    setMarkdown(e.target.value)
-  }
-  
-  const changeLoadingVar = () => {
-    setLoading(false)
-  }
-
-  useEffect(() => {
-    setTimeout(changeLoadingVar,3000)
-  },[]);
-
-    if(loading){
-      return (<div><h1>Loading...</h1></div>)
-    }
- 
-    return(
-      <div className="app">
-        <textarea onChange={handleChange}/>
-        <ReactMarkdown className="preview" children={markdown}/>
-      </div>
-    )
+function App() {
+  return (
+    <div className="app">
+      <h1>📝 Markdown Editor</h1>
+      <MarkdownEditor />
+    </div>
+  );
 }
-
-// class App extends React.Component{
-
-//   constructor(props){
-//     super(props)
-//     this.state = {
-//       markdown: "",
-//       loading: true
-//     }
-//     this.handleChange = this.handleChange.bind(this);
-//     this.changeLoadingVar = this.changeLoadingVar.bind(this);
-//   }
-//   handleChange(e){
-//     this.setState({
-//       markdown: e.target.value
-//     })
-//   }
-//   changeLoadingVar(){
-//     this.setState({
-//       loading: false
-//     })
-//   }
-
-//   componentDidMount(){
-//     setTimeout(this.changeLoadingVar,3000)
-//   }
-//   render(){
-//     if(this.state.loading){
-//       return (<div><h1>Loading...</h1></div>)
-//     }
- 
-//     return(
-//       <div className="app">
-//         <textarea onChange={this.handleChange}/>
-//         <ReactMarkdown className="preview" children={this.state.markdown}/>
-//       </div>
-//     )
-//   }
-// }
 
 export default App;
